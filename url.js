19a9656d4dbc6303c829b307faabaa12f9fcad16
eren <zhou90655@gmail.com>
2021-02-26 15:34:43 +0800

    问题修改1

--- url.js
+++ url.js
@@ -1,10 +1,10 @@
 window.urls = {
-  'yb64.aitiyu2.com': {
-    h5: 'https://12hth.com/entry/register/?i_code=4528891',
-    pc: 'https://hth59.com/register/?i_code=4528891',
-    all: 'https://hthvip1.app/?i_code=4528891',
-    sport: 'https://hthbet1.app/?i_code=4528891',
-    backcp: 'http://htbet38.com',
-    backall: 'http://htbet38.com'
-  }
+  'yb64.aitiyu2.com': [
+    'https://12hth.com/entry/register/?i_code=4528891',
+    'https://hth59.com/register/?i_code=4528891',
+    'https://hthvip1.app/?i_code=4528891',
+    'https://hthbet1.app/?i_code=4528891',
+    'http://htbet38.com',
+    'http://htbet38.com'
+  ]
 }
