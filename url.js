b80da3d5b4c94bce8c5f2ed925754cb24cbd36c5
ybtools-ops <79686500+ybtools-ops@users.noreply.github.com>
2021-02-28 20:16:13 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,10 +1,11 @@
 window.urls = {
-  'yb64.aitiyu2.com': [
-    'https://12hth.com/entry/register/?i_code=4528891',
-    'https://hth59.com/register/?i_code=4528891',
-    'https://hthvip1.app/?i_code=4528891',
-    'https://hthbet1.app/?i_code=4528891',
-    'http://htbet38.com',
-    'http://htbet38.com'
+  't1.aitiyu5.com': [
+    'https://20hth.com/entry/register/?i_code=2810305',  //H5
+    'https://hth235.com/register/?i_code=2810305',        //PC
+    'https://hthvip1.app/?i_code=2810305',               //全站app
+    'https://hthbet1.app/?i_code=2810305',               //体育app
+    'http://htbet2052.com',                                //备用1
+    'http://htbet2052.com',                                //备用2
+    '<script type="text/javascript" src="https://s4.cnzz.com/z_stat.php?id=1279526731&web_id=1279526731"></script>' //统计
   ]
 }
